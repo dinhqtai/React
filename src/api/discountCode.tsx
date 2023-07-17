85fc6ca91af4d28e72ce451ceceb8ce91ebf158f
dinhqtai <dinhquoctai2003@gmail.com>
2023-07-17 19:39:47 +0700

    fix

---
+++ src/api/discountCode.tsx
@@ -0,0 +1,18 @@
+import instance from ".";
+// export const getAllcategory = () => {
+//     const uri = "/category/"
+//     return instance.get(uri)
+// }
+// export const getByIdcategory = (id: String) => {
+//     const uri = "/category/" + id
+//     return instance.get(uri)
+// }
+// export const deleteIdcategory = (id: String) => {
+//     return instance.delete("/category/" + id)
+// }
+// export const postIdcategory = (data:) => {
+//     return instance.post("/category/", data)
+// }
+// export const putIdcategory = (id: String) => {
+//     return instance.put("/category/" + id)
+// }
