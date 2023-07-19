d8cc4fcad300aba9c4abc8a579eda0a60ed3358a
Thanh van <thanhvandtv1611@gmail.com>
2023-07-19 13:57:16 +0700

    login & register giao diện

--- src/users/login.tsx
+++ src/users/login.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import * as Yup from "yup"
+import { Link } from 'react-router-dom'
 import { useNavigate } from 'react-router-dom'
 import { useForm } from "react-hook-form"
 import { yupResolver } from '@hookform/resolvers/yup';
@@ -33,25 +34,34 @@ const Login = () => {
 
         }
     }
-    return <>
-        <div className='flex h-[350px] w-[700px] mx-auto mt-[150px] rounded-2xl bg-red-500'>
-            <form action="" className='w-[500px]'>
-                <div className='mx-[100px]'>
-                    <div className='mt-[60px]'>Email</div>
-                    <input {...register("email")} className='w-[300px] h-[30px] focus:outline-none focus:border-sky-500 focus:ring-1  focus:ring-sky-500 border border-slate-300 rounded-md' type="text" />
-                    <div>Mật khẩu</div>
-                    <input {...register("password")} className='w-[300px] h-[30px] focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 border border-slate-300 rounded-md' type="text" />
-                    <div>
-                        <button onClick={handleSubmit(onSubmit)} className='mt-[10px] w-[300px] h-[35px] bg-red-500'>Đăng nhập</button>
+    return <div className='flex justify-center pt-[50px]'>
+        <div className="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
+            <form className="space-y-6" action="#">
+                <h5 className="text-xl font-medium text-gray-900 dark:text-white">Sign in to our platform</h5>
+                <div>
+                    <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                    <input {...register("email")} type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="name@company.com" required />
+                </div>
+                <div>
+                    <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
+                    <input {...register("password")} type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required />
+                </div>
+                <div className="flex items-start">
+                    <div className="flex items-start">
+                        <div className="flex items-center h-5">
+                            <input id="remember" type="checkbox" value="" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" required />
+                        </div>
+                        <label htmlFor="remember" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember me</label>
                     </div>
+                    <a href="#" className="ml-auto text-sm text-blue-700 hover:underline dark:text-blue-500">Lost Password?</a>
+                </div>
+                <button onClick={handleSubmit(onSubmit)} type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login to your account</button>
+                <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
+                    Not registered? <Link to="/register"><a href="" className="text-blue-700 hover:underline dark:text-blue-500" >Create account</a></Link>
                 </div>
             </form>
-
-            <div className='bg-stone-100 w-[200px] rounded-r-2xl'>
-                <img className='w-[100px] mx-auto mt-[120px]' src="./logo.png" alt="" />
-            </div>
         </div>
-    </>
+    </div>
 }
 
 export default Login
--- src/users/register.tsx
+++ src/users/register.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import * as Yup from "yup"
+import { Link } from 'react-router-dom'
 import { useNavigate } from 'react-router-dom'
 import { useForm } from "react-hook-form"
 import { yupResolver } from '@hookform/resolvers/yup';
 import { signupUsers } from '../api/users'
-import {  SignupForm, signupSchema } from '../models'
+import { SignupForm, signupSchema } from '../models'
 const Register = () => {
     const { register, handleSubmit, formState: { errors } } = useForm<SignupForm>({
         resolver: yupResolver(signupSchema)
@@ -19,48 +20,52 @@ const Register = () => {
         }
 
     }
-    return <>
-        <div className='flex h-[400px] w-[700px] mx-auto mt-[150px] rounded-2xl bg-red-100'>
-            <form action="" className='w-[500px]'>
-                <div className='mx-[100px]'>
-                    <div className='mt-[40px]'>Name</div>
-                    <input {...register('name')} className='w-[300px] h-[30px] focus:outline-none focus:border-sky-500 border border-slate-300 rounded-md focus:ring-1 focus:ring-sky-500 border ' type="text" />
+    return <div className='flex justify-center pt-[50px]'>
+        <div className="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
+            <form className="space-y-6" action="#">
+                <h5 className="text-xl font-medium text-gray-900 dark:text-white">Create account</h5>
+                <div>
+                    <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your name</label>
+                    <input {...register("name")} type="text" name="name" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="Nguyen Van A" required />
                     <div className='text-red-600  text-[10px]'>
                         {errors.name && errors.name.message}
                     </div>
-                    <div className=''>Email</div>
-                    <input {...register('email')} className='w-[300px] h-[30px] focus:outline-none focus:border-sky-500 border border-slate-300 rounded-md focus:ring-1 focus:ring-sky-500 border ' type="text" />
+                </div>
+                <div>
+                    <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                    <input {...register("email")} type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="name@company.com" required />
                     <div className='text-red-600  text-[10px]'>
                         {errors.email && errors.email.message}
                     </div>
-                    <div>Số điện thoại</div>
-                    <input  {...register('phone')} className='w-[300px] h-[30px] focus:outline-none focus:border-sky-500 border border-slate-300 rounded-md focus:ring-1 focus:ring-sky-500 border ' type="text" />
+                </div>
+                <div>
+                    <label htmlFor="phone" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your phone</label>
+                    <input {...register("phone")} type="text" name="phone" id="phone" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="0123 456 789" required />
                     <div className='text-red-600  text-[10px]'>
                         {errors.phone && errors.phone.message}
                     </div>
-                    <div>Mật khẩu</div>
-                    <input {...register('password')} className='w-[300px] h-[30px] focus:outline-none focus:border-sky-500 border border-slate-300 rounded-md focus:ring-1 focus:ring-sky-500 border ' type="text" />
+                </div>
+                <div>
+                    <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
+                    <input {...register("password")} type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required />
                     <div className='text-red-600  text-[10px]'>
                         {errors.password && errors.password.message}
                     </div>
-                    <div>Nhập lại mật khẩu</div>
-                    <input {...register('confirmPassword')} className='w-[300px] h-[30px] focus:outline-none focus:border-sky-500 border border-slate-300 rounded-md focus:ring-1 focus:ring-sky-500 border ' type="text" />
+                </div>
+                <div>
+                    <label htmlFor="confirmPassword" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Enter confirm password</label>
+                    <input {...register("confirmPassword")} type="password" name="confirmPassword" id="confirmPassword" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required />
                     <div className='text-red-600  text-[10px]'>
                         {errors.confirmPassword && errors.confirmPassword.message}
                     </div>
-                    {/* <input {...register('role')} className='hidden' type="text" value={"member"} /> */}
-                    <div>
-
-                        <button type='submit' onClick={handleSubmit(onSubmit)} className='mt-[20px] w-[300px] h-[35px] bg-red-500'>Đăng nhập</button>
-                    </div>
+                </div>
+                <button onClick={handleSubmit(onSubmit)} type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Sign up</button>
+                <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
+                    Already have an account? <Link to="/login"><a href="" className="text-blue-700 hover:underline dark:text-blue-500" >Sign in</a></Link>
                 </div>
             </form>
-
-            <div className='bg-stone-100 w-[200px] rounded-r-2xl'>
-                <img className='w-[100px] mx-auto mt-[120px]' src="./logo.png" alt="" />
-            </div>
         </div>
-    </>
+    </div>
 }
 
 export default Register
