0c63cc24fbda1bb358984a435d7c5342b7c38ba2
dinhqtai <dinhquoctai2003@gmail.com>
2023-07-21 10:47:53 +0700

    up

--- src/users/shop.tsx
+++ src/users/shop.tsx
@@ -1,10 +1,13 @@
 import { useEffect, useState } from "react"
 import { IProduct } from "../models"
-import { getAll, searchPriceSort, } from "../api/products"
+import { getAll, searchPriceSort, searchProductsName, } from "../api/products"
 import Product from "../components/products"
+import { ISearchProductName } from "../model/products"
+import { useForm } from "react-hook-form"
 const Shop = () => {
   const [products, setProducts] = useState<IProduct[]>([])
   const [mess, setMess] = useState("")
+  const { register, handleSubmit } = useForm<ISearchProductName>({})
   useEffect(() => {
     const fetchProduct = async () => {
       try {
@@ -25,22 +28,34 @@ const Shop = () => {
       console.log(err);
     }
   }
+  const onSubmit = async (data: ISearchProductName) => {
+    try {
+      const search = await searchProductsName(data)
+      if (search.data.checkSearchName.length === 0) {
+        setMess("Sản phẩm không tồn tại")
+        setProducts([])
+      } else {
+        setProducts(search.data.checkSearchName)
+      }
+    } catch (errors) {
+      console.log(errors)
+    }
+
+  }
   return <>
     <div className="max-w-screen-xl px-4 mx-auto lg:px-12 w-full mt-10 ">
       <div className="relative bg-white shadow-md dark:bg-gray-800 sm:rounded-lg">
         <div className="flex flex-col items-center justify-between p-4 space-y-3 md:flex-row md:space-y-0 md:space-x-4">
           <div className="w-full md:w-1/2 ">
             <form className="flex items-center">
-              <label className="sr-only">Search</label>
+              <button type='submit' className="sr-only" onClick={handleSubmit(onSubmit)}>Search</button>
               <div className="relative w-full">
                 <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                   <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                    <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd" />
+                    <path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd" />
                   </svg>
                 </div>
-                <form action="">
-                  <input type="text" id="simple-search" className="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Tìm kiếm sản phẩm" ></input>
-                </form>
+                <input type="text" {...register("name")} id="simple-search" className=" block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Tìm kiếm sản phẩm" ></input>
               </div>
             </form>
           </div>
